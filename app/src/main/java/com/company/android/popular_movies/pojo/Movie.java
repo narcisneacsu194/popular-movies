80bbeb6c04d7adeece7eb5b615b7f99885605743
Narcis Neacsu <narcisneacsu194@gmail.com>
2017-11-15 21:32:11 +0200

    Made the Movie pojo class implement the Parcelable interface.

--- app/src/main/java/com/company/android/popular_movies/pojo/Movie.java
+++ app/src/main/java/com/company/android/popular_movies/pojo/Movie.java
@@ -1,6 +1,9 @@
 package com.company.android.popular_movies.pojo;
 
-public class Movie {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable{
     private String originalTitle;
 
     private String posterPath;
@@ -11,6 +14,43 @@ public class Movie {
 
     private String releaseDate;
 
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(originalTitle);
+        parcel.writeString(posterPath);
+        parcel.writeString(overview);
+        parcel.writeDouble(voteAverage);
+        parcel.writeString(releaseDate);
+    }
+
+    private Movie(Parcel in){
+        originalTitle = in.readString();
+        posterPath = in.readString();
+        overview = in.readString();
+        voteAverage = in.readDouble();
+        releaseDate = in.readString();
+    }
+
+
+    public Movie(){}
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR =
+            new Parcelable.Creator<Movie>(){
+                @Override
+                public Movie createFromParcel(Parcel parcel) {
+                    return new Movie(parcel);
+                }
+
+                @Override
+                public Movie[] newArray(int i) {
+                    return new Movie[i];
+                }
+            };
 
     public String getOriginalTitle() {
         return originalTitle;
